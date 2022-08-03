c086c2cfb796beb1c45405df45bf1b2705e9ea2c
askar <mr.askar1984@gmail.com>
2022-08-02 18:29:08 -0700

    first

---
+++ main.js
@@ -0,0 +1,20 @@
+let date = document.getElementById('date')
+let pnum = document.getElementById('pnum');
+let wnum = document.getElementById('wnum');
+let hnum = document.getElementById('hnum');
+let submit = document.getElementById('Submit')
+
+
+newFunction();
+
+function newFunction() {
+    console.log(date, pnum, wnum, hnum);
+}
+// create case
+// save localstorge
+// clear inputes
+// read 
+// delete
+// update
+// search
+// clea
